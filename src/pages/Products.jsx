6b3ce24fb89c8c811dd93da68b5fa0bb5c537644
stanley <stanleyonahu@gmail.com>
2026-02-11 16:02:55 +0100

    products page UI fix

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react"
 import ProductsGrid from "../components/products/ProductsGrid";
 
 const Products = () => {
-    const [loading, setLoading] = useState(true);
 
     return (
-        <div className="min-h-screen">
-            <h1 className="text-xl md:text-2xl font-semibold mb-4 font-['Poppins'] pt-4">All Products</h1>
+        <div className="min-h-screen py-16">
+            <h1 className="text-xl md:text-2xl font-semibold mb-4 font-['Poppins'] pt-8">All Products</h1>
             <ProductsGrid />
         </div>
     )

a87398796f6dc6dc7be8266ed4841f01e6fe85ed
Katchy1942 <stanleyonahu@gmail.com>
2026-04-16 10:46:04 +0100

    add orders page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter, createRoutesFromElements, RouterProvider ,Route} from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 
 //Pages
 import Home from './pages/Home';
@@ -71,88 +71,88 @@ import Products from './pages/Products';
 
 
 const App = () => {
-    const router = createBrowserRouter(createRoutesFromElements(
-        <>
+   const router = createBrowserRouter(createRoutesFromElements(
+      <>
 
-            <Route path='/' element={<BaseLayout />} errorElement={<Error />}>
-                <Route index element={<Home />}></Route>
+         <Route path='/' element={<BaseLayout />} errorElement={<Error />}>
+            <Route index element={<Home />}></Route>
 
-                {/* Auth routes */}
-                <Route path='sell-on-hezmart' element={<SellOnHezmart />}></Route>
-                <Route path='vendor-register' element={<VendorRegister />}></Route>
-                <Route path='customer-register' element={<CustomerRegister />}></Route>
-                <Route path='confirm-email' element={<ConfirmEmail />}></Route>
-                <Route path='login' element={<Login />}></Route>
-                <Route path='forgot-password' element={<ForgotPassword />}></Route>
-                <Route path='resetPassword' element={<ResetPassword />}></Route>
-            
-                <Route path="/auth/callback" element={<AuthCallback />} />
+            {/* Auth routes */}
+            <Route path='sell-on-hezmart' element={<SellOnHezmart />}></Route>
+            <Route path='vendor-register' element={<VendorRegister />}></Route>
+            <Route path='customer-register' element={<CustomerRegister />}></Route>
+            <Route path='confirm-email' element={<ConfirmEmail />}></Route>
+            <Route path='login' element={<Login />}></Route>
+            <Route path='forgot-password' element={<ForgotPassword />}></Route>
+            <Route path='resetPassword' element={<ResetPassword />}></Route>
 
-                <Route path='shops' element={<Shops />}></Route>
-                <Route path='products' element={<Products />}></Route>
-                <Route path='returns-refunds-policy' element={<ReturnsRefundsPolicy />} />
-                <Route path='privacy-policy' element={<PrivacyPolicy />} />
-                <Route path='contact' element={<Contact />} />
+            <Route path="/auth/callback" element={<AuthCallback />} />
 
-                {/* Customer Routes */}
-                {/* <Route path='orders' element={<OrderPage />}></Route>
-                <Route path='tracker/:id' element={<OrderTracker />}></Route> */}
-                <Route path='wishlist' element={<WishListPage />}></Route>
-                <Route path='orders/:id' element={<OrderDetails />}></Route>
-                <Route path='profile' element={<Profile />}></Route>
-                {/* <Route path='delivery' element={<Delivery />}></Route>
+            <Route path='shops' element={<Shops />}></Route>
+            <Route path='products' element={<Products />}></Route>
+            <Route path='returns-refunds-policy' element={<ReturnsRefundsPolicy />} />
+            <Route path='privacy-policy' element={<PrivacyPolicy />} />
+            <Route path='contact' element={<Contact />} />
+
+            {/* Customer Routes */}
+            <Route path='orders' element={<OrderPage />}></Route>
+            {/* <Route path='tracker/:id' element={<OrderTracker />}></Route> */}
+            <Route path='wishlist' element={<WishListPage />}></Route>
+            <Route path='orders/:id' element={<OrderDetails />}></Route>
+            <Route path='profile' element={<Profile />}></Route>
+            {/* <Route path='delivery' element={<Delivery />}></Route>
                 <Route path='rider-details/:id' element={<RiderDetails />}></Route> */}
-                <Route path='referral' element={<Referral />}></Route>
+            <Route path='referral' element={<Referral />}></Route>
 
-                {/* Product Routes */}
-                <Route path='/product/:id' element={<ProductDetails />}></Route>
-                <Route path='/category/:categoryId/:subcategoryId?' element={<CategoryProducts />} />
-                <Route path="/vendor/:vendorId" element={<VendorProductsPage />} />
-                <Route path='cart' element={<Cart />}></Route>
-                <Route path='checkout' element={<Checkout />}
-                    loader={async({ request }) => 
-                    await requireAuth(request, ['customer', 'vendor', 'admin'])}>
-                </Route>
+            {/* Product Routes */}
+            <Route path='/product/:id' element={<ProductDetails />}></Route>
+            <Route path='/category/:categoryId/:subcategoryId?' element={<CategoryProducts />} />
+            <Route path="/vendor/:vendorId" element={<VendorProductsPage />} />
+            <Route path='cart' element={<Cart />}></Route>
+            <Route path='checkout' element={<Checkout />}
+               loader={async ({ request }) =>
+                  await requireAuth(request, ['customer', 'vendor', 'admin'])}>
+            </Route>
+         </Route>
+
+         <Route path='/manage' element={<AuthenticatedLayout />} errorElement={<Error />}>
+            {/* Admin Routes */}
+            <Route path='admin' loader={async ({ request }) => await requireAuth(request, 'admin')}>
+               <Route path='dashboard' element={<AdminDashboard />} />
+               <Route path='vendors' element={<ShopManager />} />
+               <Route path="vendors/:id" element={<VendorDetails />} />
+               <Route path='customers' element={<CustomersManager />}></Route>
+               <Route path='products' element={<AdminManageProducts />} />
+               <Route path='categories' element={<ManageCategories />}></Route>
+               <Route path='orders' element={<AdminOrdersManager />}></Route>
+               <Route path='orders/:orderId' element={<AdminOrderDetails />}></Route>
+               <Route path='coupons' element={<AdminManageCoupons />}></Route>
+               <Route path='create-coupon' element={<CreateCoupon />}></Route>
+               <Route path='edit-coupon/:id' element={<EditCoupon />}></Route>
+               <Route path='settings' element={<AdminSettings />}></Route>
+               <Route path='subscribers' element={<Subscribers />} />
             </Route>
 
-            <Route path='/manage' element={<AuthenticatedLayout />} errorElement={<Error />}>
-                {/* Admin Routes */}
-                <Route path='admin' loader={async({ request }) => await requireAuth(request, 'admin')}>
-                    <Route path='dashboard' element={<AdminDashboard />} />
-                    <Route path='vendors' element={<ShopManager />} />
-                    <Route path="vendors/:id" element={<VendorDetails />} />
-                    <Route path='customers' element={<CustomersManager />}></Route>
-                    <Route path='products' element={<AdminManageProducts />} />
-                    <Route path='categories' element={<ManageCategories />}></Route>
-                    <Route path='orders' element={<AdminOrdersManager />}></Route>
-                    <Route path='orders/:orderId' element={<AdminOrderDetails />}></Route>
-                    <Route path='coupons' element={<AdminManageCoupons />}></Route>
-                    <Route path='create-coupon' element={<CreateCoupon />}></Route>
-                    <Route path='edit-coupon/:id' element={<EditCoupon />}></Route>
-                    <Route path='settings' element={<AdminSettings />}></Route>
-                    <Route path='subscribers' element={<Subscribers />}/>
-                </Route>
-
-                {/* Vendor Routes */}
-                <Route path='vendor' loader={({ request }) => requireAuth(request, 'vendor')}>
-                    <Route path='dashboard' element={<VendorDashboard />} />
-                    <Route path='products' element={<VendorManageProducts />} />
-                    <Route path='add-product' element={<AddProduct />} ></Route>
-                    <Route path='edit-product/:id' element={<EditProduct />}></Route>
-                    <Route path='orders' element={<VendorOrdersManager />}></Route>
-                    <Route path='orders/:orderId' element={<VendorOrderDetails />}></Route>
-                    <Route path='settings' element={<VendorSettings />}></Route>
-                </Route>
+            {/* Vendor Routes */}
+            <Route path='vendor' loader={({ request }) => requireAuth(request, 'vendor')}>
+               <Route path='dashboard' element={<VendorDashboard />} />
+               <Route path='products' element={<VendorManageProducts />} />
+               <Route path='add-product' element={<AddProduct />} ></Route>
+               <Route path='edit-product/:id' element={<EditProduct />}></Route>
+               <Route path='orders' element={<VendorOrdersManager />}></Route>
+               <Route path='orders/:orderId' element={<VendorOrderDetails />}></Route>
+               <Route path='settings' element={<VendorSettings />}></Route>
             </Route>
+         </Route>
 
-            <Route path='pending_verification' element={<PendingVerification />}></Route>
-            <Route path="*" element={<NotFound />} />
-        </>
-    ))
+         <Route path='pending_verification' element={<PendingVerification />}></Route>
+         <Route path="*" element={<NotFound />} />
+      </>
+   ))
 
-    return (
-        <RouterProvider router={router} />
-    )
+   return (
+      <RouterProvider router={router} />
+   )
 }
 
 export default App
